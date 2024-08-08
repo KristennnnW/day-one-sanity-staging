f5acc25ca176a0c59132608089dbfd9f5d75e8e6
KristennnnW <89870255+KristennnnW@users.noreply.github.com>
2024-08-08 16:20:07 -0500

    update synctostaging.js

--- sync-to-staging.js
+++ sync-to-staging.js
@@ -1,44 +1,44 @@
 const { createClient } = require('@sanity/client');
-require('dotenv').config(); // Ensure dotenv is loaded to read .env file
+require('dotenv').config();
 
 const API_VERSION = '2023-01-01'; // Specify an appropriate API version
 
-const productionClient = createClient({
-  projectId: '9r4zkoar',
-  dataset: 'production',
-  token: process.env.SANITY_TOKEN,
-  apiVersion: API_VERSION, // Use the API version
-  useCdn: false,
-});
-
 const stagingClient = createClient({
-  projectId: '9r4zkoar',
+  projectId: process.env.SANITY_PROJECT,
   dataset: 'staging',
   token: process.env.SANITY_TOKEN,
-  apiVersion: API_VERSION, // Use the API version
+  apiVersion: API_VERSION,
   useCdn: false,
 });
 
-async function syncToStaging() {
-  try {
-    // Fetch all documents from the production dataset
-    const productionDocuments = await productionClient.fetch('*');
+const productionClient = createClient({
+  projectId: process.env.SANITY_PROJECT,
+  dataset: 'production',
+  token: process.env.SANITY_TOKEN,
+  apiVersion: API_VERSION,
+  useCdn: false,
+});
 
-    for (const document of productionDocuments) {
+async function syncToProduction() {
+  try {
+    // Fetch all documents from the staging dataset
+    const stagingDocuments = await stagingClient.fetch('*');
+
+    for (const document of stagingDocuments) {
       const { _id, ...rest } = document;
 
-      // Create or replace the document in the staging dataset
-      await stagingClient.createOrReplace({
+      // Create or replace the document in the production dataset
+      await productionClient.createOrReplace({
         ...rest,
         _id: _id.replace('drafts.', ''), // Remove 'drafts.' prefix if present
         _type: document._type,
       });
 
-      console.log(`Document ${_id} synced to staging`);
+      console.log(`Document ${_id} synced to production`);
     }
   } catch (error) {
     console.error('Error syncing documents:', error);
   }
 }
 
-syncToStaging();
+syncToProduction();
